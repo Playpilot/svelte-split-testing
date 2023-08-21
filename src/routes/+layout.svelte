83f0c289c4bb618a382fed1a3f1ac999fc00a824
Mitchel <mitchel_jager@hotmail.com>
2023-08-21 12:26:39 +0200

    chore: Update layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,9 @@
 <script>
-  import { clientGetSplitTestingIdentifier } from '$lib/splitTesting.js'
   import { setContext } from 'svelte'
 
   export let data
 
-  setContext('splitTestingIdentifier', data?.splitTestingIdentifier || clientGetSplitTestingIdentifier())
+  setContext('splitTestingIdentifier', data?.splitTestingIdentifier)
 </script>
 
 <slot />
