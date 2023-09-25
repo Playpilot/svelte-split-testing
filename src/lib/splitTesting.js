5450394a641ea684d447558bb75f7cee82799947
Mitchel <mitchel_jager@hotmail.com>
2023-09-25 18:16:32 +0200

    feat: Add component separate tracking

--- src/lib/splitTesting.js
+++ src/lib/splitTesting.js
@@ -1,5 +1,6 @@
 import { BROWSER } from 'esm-env'
 import { v4 as uuidv4 } from 'uuid'
+import { prng_alea as seedrandom } from 'esm-seedrandom'
 
 const oneYear = 365 * 864e5
 
@@ -67,7 +68,66 @@ function clientGetSplitTestCookie(cookieName) {
 
   return document.cookie?.split(';').reduce((r, v) => {
     const [name, value] = v?.split('=').map(item => item.trim())
-    console.log(name, value)
     return name === cookieName ? decodeURIComponent(value) : r.trim()
   }, '')
+}
+
+/**
+ * Generate a random number based on a seed, meaning it will always be
+ * the same outcome as long as the identifier is the same..
+ * The key is also included to prevent one user from always seeing test A
+ * for every test case.
+ * @param {object} options - Optional parameters.
+ * @param {string} options.key - Key used to identify the current test
+ * @param {string[]} options.variants - Array of strings with all possible variants
+ * @param {string} options.identifier - Identifier used to find the current variants. Either from cookie or from user identifier.
+ * @param {string} options.force - Force a particular split test by string.
+ * @returns {string} The variant to be used
+ */
+export function getVariant({ key, variants = [], identifier, force }) {
+  const randomized = seedrandom(identifier + key).quick()
+  const index = Math.floor(randomized * variants.length)
+  return force && variants.includes(force) ? force : variants[index]
+}
+
+/**
+ * Perform a split test outside of a component. The variant is generated and returned from this function. It will perform a view action when called.
+ * @param {object} options - Optional parameters.
+ * @param {string} options.key - Key used to identify the current test
+ * @param {string} options.action - Action send to analytics tracking
+ * @param {string[]} options.variants - Array of strings with all possible variants
+ * @param {string|null} options.userIdentifier - Optional user identifier to override the cookie identifier
+ * @param {string} options.force - Force a particular split test by string.
+ * @param {Function} options.trackingFunction - Function to override the default GTM data layer tracking. `{ action, key, variant }` is passed as the first and only parameter.
+ * @returns {string} The variant that was used
+ * @example `
+ * const variant = performSplitTestAction({ ... })
+ * if (variant === "A") doThingA()
+ * else if (variant === "B") doThingB()
+ */
+export function performSplitTestAction({ key = '', action = 'view', variants = [], userIdentifier = null, force = null, trackingFunction = null } = {}) {
+  if (!BROWSER) return
+
+  const identifier = clientGetSplitTestIdentifier({ userIdentifier })
+  const variant = getVariant({ key, variants, identifier, force })
+
+  typeof trackingFunction === "function" ? trackingFunction({ action, key, variant }) : sendToDataLayer({ action, key, variant })
+
+  return variant
+}
+
+/**
+ * Send an event to GTM data layer
+ * @param {object} options - Parameters.
+ * @param {string} options.action - Action send to analytics tracking
+ * @param {string} options.key - Key used to identify the current test
+ * @param {string} options.variant - The current variant being used
+ * @example `
+ * const variant = performSplitTestAction({ ... })
+ * if (variant === "A") doThingA()
+ * else if (variant === "B") doThingB()
+ */
+export function sendToDataLayer({ action, key, variant } = {}) {
+  window.dataLayer = window.dataLayer || []
+  window.dataLayer.push({ event: 'Split Test', action, label: key, value: variant })
 }
