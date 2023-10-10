0bc74e261496a5601266f3a5eed786b30c181af1
Mitchel <mitchel_jager@hotmail.com>
2023-10-10 12:47:20 +0200

    chore: Add more functions to export

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,4 +1,4 @@
 import SplitTest from "./SplitTest.svelte"
-import { serverGetSplitTestIdentifier, clientGetSplitTestIdentifier } from "./splitTesting.js"
+import { serverGetSplitTestIdentifier, clientGetSplitTestIdentifier, getVariant, performSplitTestAction } from "./splitTesting.js"
 
-export { SplitTest, serverGetSplitTestIdentifier, clientGetSplitTestIdentifier }
+export { SplitTest, serverGetSplitTestIdentifier, clientGetSplitTestIdentifier, getVariant, performSplitTestAction }
