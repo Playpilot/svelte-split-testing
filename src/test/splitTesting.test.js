41343fe8bd7a77f196f1659ed56f1954a475c630
Mitchel <mitchel_jager@hotmail.com>
2024-12-06 18:51:27 +0100

    chore: Upgrade to Svelte 5

--- src/test/splitTesting.test.js
+++ src/test/splitTesting.test.js
@@ -5,11 +5,11 @@ import { performSplitTestAction } from '../lib/splitTesting.js'
 describe('splitTesting.js', () => {
   describe('performSplitTestAction', () => {
     it('Should return the expected variant', () => {
-      expect(performSplitTestAction({ key: 'A', variants: ['Variant A', 'Variant B'] })).toBe('Variant A')
+      expect(performSplitTestAction({ key: 'a', variants: ['Variant A', 'Variant B'] })).toBe('Variant A')
     })
 
     it('Should return the forced variant regardless of key', () => {
-      expect(performSplitTestAction({ key: 'A', variants: ['Variant A', 'Variant B'], force: 'Variant B' })).toBe('Variant B')
+      expect(performSplitTestAction({ key: 'a', variants: ['Variant A', 'Variant B'], force: 'Variant B' })).toBe('Variant B')
     })
 
     it('Should call the given function', () => {
